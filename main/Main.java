4b848f69511ff26d31c9a1609741e52956880a04
qk8333 <evan.smith.m@gmail.com>
2018-05-11 03:57:17 -0700

    Uploaded Main file
    As you can guess, this short file simply runs everything.

---
+++ main/Main.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import controller.Controller;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Evan Smith
+ */
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);  
+    }
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Controller controller = new Controller(primaryStage);
+        controller.start();
+    }
+    
+}
